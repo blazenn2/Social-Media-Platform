e436dac886f76903c95518060f661002a4020bfb
HAMZA NAWAB <hamzanawab.bs@gmail.com>
2022-09-13 01:41:22 +0500

    Pagination and deletion functionality added

--- backend/controllers/feed.js
+++ backend/controllers/feed.js
@@ -6,10 +6,14 @@ const fs = require('fs');
 // <=========================== GET ALL POSTS FROM DB ===========================> //
 exports.getPosts = async (req, res, next) => {
   try {
-    const getPosts = await post.find();
+    const currentPage = req.query.page || 1;
+    const postPerPage = 2;
+    const totalPost = await post.countDocuments();
+    const getPosts = await post.find().skip((currentPage - 1) * postPerPage).limit(postPerPage);
     if (getPosts) res.status(200).json({
       message: 'Fetched posts successfully',
-      posts: getPosts
+      posts: getPosts,
+      totalItems: totalPost
     });
   } catch (err) {
     err.statusCode = 422;
@@ -83,12 +87,31 @@ exports.updatePost = async (req, res, next) => {
       error.statusCode = 404;
       throw error;
     }
-    console.log(req.body.imageUrl);
     getPost.title = req.body.title;
     getPost.content = req.body.content;
     if (imageUrl !== getPost.imageUrl) clearImage(getPost.imageUrl);
     getPost.imageUrl = imageUrl;
     return getPost.save();
+  } catch (err) {
+    next(err);
+  }
+};
+
+
+exports.deletePost = async (req, res, next) => {
+  try {
+    const postId = req.params.postId;
+    const getPost = await post.findById(postId);
+    if (!getPost) {
+      const error = new Error(`Couldn't find post.`);
+      error.statusCode = 404;
+      throw error;
+    } else {
+      const deletedPost = await post.findByIdAndRemove(postId);
+      clearImage(getPost.imageUrl);
+      console.log(deletedPost);
+      return res.status(200).json({ message: "Deleted post successfully", });
+    }
   } catch (err) {
     next(err);
   }
