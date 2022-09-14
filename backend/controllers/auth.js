4921c8122d74f180658dd4e0bf1ad95c76bcef08
HAMZA NAWAB <hamzanawab.bs@gmail.com>
2022-09-14 23:09:59 +0500

    Bcrypt injection

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,5 +1,6 @@
 const { validationResult } = require('express-validator');
 const user = require('../models/user');
+const bycrypt = require('bcryptjs');
 
 exports.signup = async (req, res, next) => {
     try {
@@ -10,6 +11,8 @@ exports.signup = async (req, res, next) => {
         const name = req.body.name;
         const email = req.body.email;
         const password = req.body.password;
+        const encryptedPassword = await bycrypt.hash(password, 12);
+        
     } catch (err) {
         next(err);
     }
